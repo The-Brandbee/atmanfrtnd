7a6d5b832a398f5d16f4e6b2f44c2055a02b5a6b
The-Brandbee <digital@thebrandbee.com>
2025-03-05 14:27:08 +0530

    new

---
+++ .next/static/webpack/app/page.415a2d66b22ce62f.hot-update.js
@@ -0,0 +1,22 @@
+"use strict";
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+self["webpackHotUpdate_N_E"]("app/page",{
+
+/***/ "(app-pages-browser)/./src/app/img/3d-2.png":
+/*!******************************!*\
+  !*** ./src/app/img/3d-2.png ***!
+  \******************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval(__webpack_require__.ts("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = ({\"src\":\"/_next/static/media/3d-2.e57f688a.png\",\"height\":249,\"width\":249,\"blurDataURL\":\"/_next/image?url=%2F_next%2Fstatic%2Fmedia%2F3d-2.e57f688a.png&w=8&q=70\",\"blurWidth\":8,\"blurHeight\":8});//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiKGFwcC1wYWdlcy1icm93c2VyKS8uL3NyYy9hcHAvaW1nLzNkLTIucG5nIiwibWFwcGluZ3MiOiI7Ozs7QUFBQSxpRUFBZSxDQUFDLDRMQUE0TCIsInNvdXJjZXMiOlsiRDpcXGF0bmFtXFxzcmNcXGFwcFxcaW1nXFwzZC0yLnBuZyJdLCJzb3VyY2VzQ29udGVudCI6WyJleHBvcnQgZGVmYXVsdCB7XCJzcmNcIjpcIi9fbmV4dC9zdGF0aWMvbWVkaWEvM2QtMi5lNTdmNjg4YS5wbmdcIixcImhlaWdodFwiOjI0OSxcIndpZHRoXCI6MjQ5LFwiYmx1ckRhdGFVUkxcIjpcIi9fbmV4dC9pbWFnZT91cmw9JTJGX25leHQlMkZzdGF0aWMlMkZtZWRpYSUyRjNkLTIuZTU3ZjY4OGEucG5nJnc9OCZxPTcwXCIsXCJibHVyV2lkdGhcIjo4LFwiYmx1ckhlaWdodFwiOjh9OyJdLCJuYW1lcyI6W10sImlnbm9yZUxpc3QiOltdLCJzb3VyY2VSb290IjoiIn0=\n//# sourceURL=webpack-internal:///(app-pages-browser)/./src/app/img/3d-2.png\n"));
+
+/***/ })
+
+});
---
+++ src/app/common/Footer.js
@@ -0,0 +1,117 @@
+import Head from "next/head";
+import Link from "next/link";
+import Image from "next/image";
+import Script from "next/script";
+import Logo from "../img/Layer_1.svg";
+import instagram from "../img/instagram_1400829.svg";
+import facebook from "../img/facebook_733603.svg";
+import Layer from "../img/Layer-2.svg";
+function IndexPage() {
+  return (
+    <footer>
+   
+<section className="footer">
+    <div className="container">
+        <div className="row">
+            <div className="col-lg-3 col-md-3 col-sm-12">
+               <Link href="/">
+                <Image src={Logo} alt="" className="footer_logo w-100" />
+                </Link>
+            </div>
+        </div>
+        <div className="row">
+            <div className="col-lg-4 col-md-4 col-sm-12">
+                <div>
+                    <div className="row">
+                        <div className="col-lg-9 col-md-9 col-sm-9 col-9">
+                            <input type="text" className="footer_input"/>
+                        </div>
+                        <div className="col-lg-3 col-md-3 col-sm-3 col-3">
+                            <button className="request_btn" >Submit</button>
+                        </div>
+                        <div className="col-lg-12 col-md-12 col-sm-12">
+                            <div className="touch_box">
+                                <p className="touch_text">Keep in touch</p>
+                                <ul className="d-flex">
+                                    <li className="social_link">
+                                        <a href="#">
+                                           
+                                            <Image src={instagram} alt="" />
+                                        </a>
+                                    </li>
+                                    <li className="social_link">
+                                        <a href="#">
+                                           
+                                            <Image src={facebook} alt=""  />
+                                        </a>
+                                    </li>
+                                    <li className="social_link">
+                                        <a href="#">
+                                         
+                                            <Image src={Layer} alt=""  />
+                                        </a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="col-lg-8 col-md-8 col-sm-12">
+                <div className="links-box">
+                    <div className="row">
+                        <div className="col-lg-4 col-md-4 col-sm-6 col-6">
+                            <h6 className="quick_head">USE-CASES</h6>
+                             <ul className="">
+                                <li className="foot-item">
+                                    <a href="#" class="foot-links">Training & Development</a>
+                                </li>
+                                <li className="foot-item">
+                                    <a href="#" class="foot-links">Sales & Marketing</a>
+                                </li>
+                             </ul>
+                        </div>
+                        <div className="col-lg-4 col-md-4 col-sm-6 col-6">
+                            <h6 className="quick_head">FEATURES</h6>
+                             <ul className="">
+                                <li className="foot-item">
+                                    <a href="#" class="foot-links">AI Avatars</a>
+                                </li>
+                                <li className="foot-item">
+                                    <a href="#" class="foot-links">Text-to-Video</a>
+                                </li>
+                             </ul>
+                        </div>
+                        <div className="col-lg-4 col-md-4 col-sm-6">
+                            <h6 className="quick_head">COMPANY</h6>
+                             <ul className="">
+                                <li className="foot-item">
+                                    <a href="#" class="foot-links">About Us</a>
+                                </li>
+                                <li className="foot-item">
+                                    <a href="#" class="foot-links">Contact Us</a>
+                                </li>
+                                <li className="foot-item">
+                                    <a href="#" class="foot-links">Affiliate</a>
+                                </li>
+                                <li className="foot-item">
+                                    <a href="#" class="foot-links">Partners</a>
+                                </li>
+                             </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="col-lg-12 col-md-12 col-sm-12">
+                <div>
+                    <p className="copyright">Copyright@ <span className="atnam">Atman.ai</span></p>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+    </footer> 
+  );
+}
+
+export default IndexPage;
